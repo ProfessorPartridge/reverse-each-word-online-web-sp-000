030028a6026e563b81253ab19bce246dd15ad9ea
Josiah  Partridge <jpartridj94@gmail.com>
2020-03-10 03:35:40 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ def reverse_each_word(sentence)
   new_array_format = []
   array_format = sentence.split
   array_format.collect do |word|
-    new_array_format = word.reverse
+    new_array_format << word.reverse
   end
   new_array_format.join(" ")
 end
