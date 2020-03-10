0f4fdae3f3f7690ff1f68964124ecd17abc6cf5b
Josiah  Partridge <jpartridj94@gmail.com>
2020-03-10 03:31:40 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,6 +2,6 @@ def reverse_each_word(sentence)
   array_format = sentence.split
   array_format.collect do |word|
     new_array_format = word.reverse
+    new_array_format.join(" ")
   end
-  new_array_format.join(" ")
 end
